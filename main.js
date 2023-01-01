2ceb961d9c88f2af3556144ad89d59d4a1f76cfc
ahmedfarag9 <ahmedfarag.a12@gmail.com>
2023-01-01 02:14:18 +0200

    add func to show/hide faq answers

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ window.addEventListener('scroll', () => {
 
 
 
+// show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon when clicked
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus'
+        } else {
+            icon.className = 'uil uil-plus'
+        }
+
+    })
+})
